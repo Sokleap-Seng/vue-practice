bacf7036a81c7232d148115c8a0f08874671b2ab
Sokleap-Seng <sokleaps808@gmail.com>
2025-06-18 10:40:59 +0700

    new practice

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <Navigation/>
+ <router-view></router-view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Navigation from './components/Navigation.vue'
+ 
+</script>
+
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="h-16 flex items-center shadow-md justify-between">
+        <div id="nav-item" class="flex items-center m-auto gap-12">
+            <navigation-item v-for="item in navItems" :key="item.label" :item="item">
+            </navigation-item>
+        </div>
+        <div id="nav-item" class="flex items-center m-auto gap-12">
+            <navigation-item :item="{ label: 'Login', path: '/login', icon: 'login' }"></navigation-item>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import NavigationItem from '../components/NavigationItem.vue'
+
+const navItems = ref([
+    { label: "Home", path: "/", icon: "home" },
+    { label: "About", path: "/about", icon: "menu_book" },
+    { label: "Contact Us", path: "/contact", icon: "shopping_cart_checkout" },
+   
+])
+
+</script>
+
+<style scoped></style>
+
+
